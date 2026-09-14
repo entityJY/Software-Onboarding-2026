052bb11513f62e5e4db607afa2ab9134a43399c6
entityJY <jomathya07@gmail.com>
2026-09-14 18:26:09 +0000

    refactor: Remove Feedback protocol class, add in TypeAliases for Feedback and Result

--- ros_tutorials/src/actions/actions/client_helper.py
+++ ros_tutorials/src/actions/actions/client_helper.py
@@ -1,18 +1,16 @@
 from collections.abc import Callable
-from typing import Protocol, Any
+from typing import Any, TypeAlias
 from functools import wraps
 
 from rclpy.action.client import ClientGoalHandle
 from rclpy.task import Future
 
-class Feedback(Protocol):
-    __slots__: list[str]
-    _fields_and_field_types: dict[str, str]
-    
-    @classmethod
-    def get_fields_and_field_types(cls) -> dict[str, str]: ...
 
-def goal_feedback(func: Callable[[Any, Any], None]):
+Feedback: TypeAlias = Any
+Result: TypeAlias = Any
+
+
+def goal_feedback(func: Callable[[Any, Feedback], None]):
     @wraps(func)
     def wrapper(self, feedback_msg):
         feedback = feedback_msg.feedback
@@ -27,7 +25,7 @@ def response_feedback(func: Callable[[Any, ClientGoalHandle], None]):
         func(self, goal_handle)
     return wrapper
 
-def result_feedback(func: Callable[[Any, Any], None]):
+def result_feedback(func: Callable[[Any, Result], None]):
     @wraps(func)
     def wrapper(self, future: Future):
         res = future.result()
